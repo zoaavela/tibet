da9a3ee4bb74fc7390e77e5c06759e7cba1150f8
zoaavela <enzooabdi@gmail.com>
2026-04-04 21:40:14 +0200

    head html page

--- vue/src/data/museumData.js
+++
@@ -1,6 +0,0 @@
-// museumData.js
-export const ARTICLES = [
-    { id: "atlas", label: "L'Atlas", path: "/atlas" },
-    { id: "society", label: "Society", path: "/society" },
-    { id: "chronicles", label: "Chronicles", path: "/chronicles" }
-];
